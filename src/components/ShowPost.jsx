84443ae3bba26ffacb79929f50f11a3f5dd96b3e
Ahyoung-Kim <eileel@naver.com>
2023-05-15 17:54:35 +0900

    최종완성본

---
+++ src/components/ShowPost.jsx
@@ -0,0 +1,151 @@
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  LoadingDiv,
+  LoadingImg,
+  PostReadDiv,
+  PostReplDiv,
+  PostSection,
+  PostTitle,
+  PostTitleDiv,
+  Repl,
+  ReplInput,
+  ReplSubmitDiv,
+  ReplTitleDiv,
+  Replwriter,
+  WritereplDiv,
+} from "../styledComponent";
+import loadingIcon from "../loading.svg";
+
+import axios from "axios";
+import { APIURL } from "../App";
+
+// const post = {
+//   id: "1",
+//   title: "제곧내",
+//   contents: "오늘 휴강인가요?",
+//   repls: [
+//     {
+//       id: "repl1",
+//       contents: "ㅇㅇ",
+//       post: "1",
+//     },
+//     {
+//       id: "repl2",
+//       contents: "yes",
+//       post: "1",
+//     },
+//   ],
+// };
+
+const PostAndRepl = React.memo(
+  ({ post, postLoading, replLoading, repls, replCount }) => {
+    return (
+      <>
+        <PostTitleDiv>
+          <PostTitle>{post && post.title}</PostTitle>
+        </PostTitleDiv>
+
+        {postLoading ? (
+          <LoadingDiv>
+            <LoadingImg src={loadingIcon} />
+          </LoadingDiv>
+        ) : (
+          <PostReadDiv>{post && post.contents}</PostReadDiv>
+        )}
+
+        <ReplTitleDiv>댓글 {replCount}</ReplTitleDiv>
+        {replLoading ? (
+          <LoadingDiv>
+            <LoadingImg src={loadingIcon} />
+          </LoadingDiv>
+        ) : (
+          repls &&
+          repls.map((repl) => (
+            <PostReplDiv key={repl.id}>
+              <Replwriter>익명</Replwriter>
+              <Repl>{repl.contents}</Repl>
+            </PostReplDiv>
+          ))
+        )}
+      </>
+    );
+  }
+);
+
+const ShowPost = () => {
+  // params
+  const params = useParams();
+
+  // 포스트
+  const [post, setPost] = useState(null);
+  // 댓글
+  const [repls, setRepls] = useState([]);
+
+  // loading states
+  const [postLoading, setPostLoading] = useState(true);
+  const [replLoading, setReplLoading] = useState(true);
+
+  // 댓글 인풋
+  const [repl, setRepl] = useState("");
+  const replInput = useRef(null);
+
+  const onChange = (e) => {
+    const { value } = e.target;
+    setRepl(value);
+  };
+
+  const getPostData = async () => {
+    const res = await axios.get(`${APIURL}/api/posts/${params.postID}/`);
+    console.log(res);
+    setPost(res.data);
+    setPostLoading(false);
+    setRepls(res.data.repls);
+    setReplLoading(false);
+  };
+
+  const onSubmitRepl = async () => {
+    try {
+      const body = {
+        contents: repl,
+        post: params.postID,
+      };
+      const res = await axios.post(`${APIURL}/api/repl/`, body);
+      console.log(res);
+      window.location.reload();
+    } catch (err) {
+      console.log("onSubmitRepl Error: ", err);
+    }
+  };
+
+  useEffect(() => {
+    getPostData();
+  }, []);
+
+  const replCount = useMemo(() => repls.length, [repls]);
+
+  if (!params.postID) {
+    return <PostSection>잘못된 접근입니다.</PostSection>;
+  }
+
+  return (
+    <PostSection>
+      <PostAndRepl
+        post={post}
+        postLoading={postLoading}
+        replCount={replCount}
+        replLoading={replLoading}
+        repls={repls}
+      />
+
+      <WritereplDiv>
+        <ReplInput onChange={onChange} value={repl} ref={replInput} />
+        <ReplSubmitDiv onClick={onSubmitRepl}>
+          <span>입력</span>
+        </ReplSubmitDiv>
+      </WritereplDiv>
+    </PostSection>
+  );
+};
+
+export default ShowPost;
---
+++ src/components/WritePost.jsx
@@ -0,0 +1,70 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { APIURL } from "../App";
+import {
+  PostSection,
+  PostSubmit,
+  PostSubmitDiv,
+  PostTitle,
+  PostWriteDiv,
+  PostTitleDiv,
+} from "../styledComponent";
+
+import InputPost from "./InputPost";
+
+const SubmitComponent = React.memo(({ onSubmit }) => (
+  <PostSubmitDiv onClick={onSubmit}>
+    <PostSubmit>작성완료</PostSubmit>
+  </PostSubmitDiv>
+));
+
+const WriteTitle = React.memo(() => (
+  <PostTitleDiv>
+    <PostTitle>글쓰기</PostTitle>
+  </PostTitleDiv>
+));
+
+const WritePost = () => {
+  const [inputs, setInputs] = useState({
+    title: "",
+    contents: "",
+  });
+  const { title, contents } = inputs;
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const navigate = useNavigate();
+  const onSubmit = () => {
+    axios
+      .post(`${APIURL}/api/posts/`, {
+        title,
+        contents,
+      })
+      .then((res) => {
+        console.log(res);
+        alert("글이 등록되었습니다!");
+        navigate("../");
+      });
+  };
+
+  return (
+    <PostSection>
+      <WriteTitle />
+
+      <PostWriteDiv>
+        <InputPost onChange={onChange} title={title} contents={contents} />
+      </PostWriteDiv>
+
+      <SubmitComponent onSubmit={onSubmit} />
+    </PostSection>
+  );
+};
+
+export default WritePost;
